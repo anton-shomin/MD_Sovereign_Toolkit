2b45d9e9c0a9b4d5c308df41b88017861a017d8c
Anton Shomin <anton_shomin@epam.com>
2025-05-07 11:01:13 +0300

    updated format

--- MD_Sovereign_Toolkit/MD_Sovereign_Toolkit/common/defines/md_defines.lua
+++
@@ -1,1 +0,0 @@
-	NDefines.NGame.END_DATE = "2956.1.1.1" -- we are called r56 for a reason!
---
+++ common/defines/md_defines.lua
@@ -0,0 +1,29 @@
+	NDefines.NGame.END_DATE = "2956.1.1.1" -- we are called r56 for a reason!
+  NDefines.NGame.END_DATE = "2956.1.1.1" -- we are called r56 for a reason!
+	NDefines.NGame.HANDS_OFF_START_TAG = "YEM" --more central than vanilla's Haiti
+	
+									-- Vanilla is -100	
+	NDefines.NDiplomacy.TENSION_VOLUNTEER_FORCE_DIVISION = 0.1
+	NDefines.NDiplomacy.MAX_REMEMBERED_LEASED_IC = 2000				-- Maximum of leased equipment value that is remembered for opinion bonus
+	NDefines.NDiplomacy.MAX_OPINION_FOR_LEASED_IC = 50					-- Positive opinion when remembering the MAX_REMEMBERED_LEASED_IC equipment
+	NDefines.NDiplomacy.MONTHLY_LEASED_IC_DECAY = 35					-- How much of leased equipment is being "forgot" each month
+	
+	NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 9999 -- Max army experience a country can store
+	NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 9999 -- Max navy experience a country can store
+	NDefines.NMilitary.MAX_AIR_EXPERIENCE = 9999  -- Max air experience a country can store
+	
+	
+	NDefines.NDiplomacy.VOLUNTEERS_PER_TARGET_PROVINCE = 0.5			-- Each province owned by the target country contributes this amount of volunteers to the limit.
+	NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 0.5				-- Each army unit owned by the source country contributes this amount of volunteers to the limit.
+	NDefines.NDiplomacy.VOLUNTEERS_RETURN_EQUIPMENT = 0.5				-- Returning volunteers keep this much equipment
+	NDefines.NDiplomacy.VOLUNTEERS_TRANSFER_SPEED = 3					-- days to transfer a unit to another nation
+	NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 10
+	
+	NDefines.NNavy.NAVAL_SUPREMACY_CAN_INVADE = 0.05	
+	NDefines.NCountry.PARADROP_AIR_SUPERIORITY_RATIO = 0.07
+
+
+  NDefines.NCountry.SUPPLY_CONVOY_FACTOR = 0.025
+	
+	NDefines.NCountry.BASE_MAX_COMMAND_POWER = 9999
+	NDefines.NCountry.POLITICAL_POWER_UPPER_CAP = 9999
